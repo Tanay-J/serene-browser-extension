268e35ff11decc05f51816263a82be9e1cdba47b
Tanay <tanay.joshi9@gmail.com>
2022-05-11 14:25:32 +0530

    feat: api requests to unsplash.com

--- src/utils/api-requests.js
+++ src/utils/api-requests.js
@@ -1,1 +1,20 @@
-// api requests
+import axios from "axios";
+
+export const getCoverImage = async (setImageData) => {
+  try {
+    const {
+      data: { urls, location, description },
+    } = await axios.get(
+      `https://api.unsplash.com/photos/random/?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`,
+      { params: { orientation: "landscape" } }
+    );
+
+    setImageData({
+      url: urls.regular,
+      location: location.name,
+      description: description,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
