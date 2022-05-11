812816fdfb10b92e4d50d7212465763171fcb89e
Tanay <tanay.joshi9@gmail.com>
2022-05-11 14:23:13 +0530

    feat, style: CoverImage and CurrentTime components

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -1,4 +1,33 @@
+import { useEffect, useState } from "react";
+
 const CurrentTime = () => {
-  return <>CurrentTime</>;
+  const [date, setDate] = useState(new Date());
+  const daysArr = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  const minutes =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  const day = daysArr[date.getDay() - 1];
+
+  useEffect(() => {
+    const timer = setInterval(() => setDate(new Date()), 60000);
+    return () => clearInterval(timer);
+  }, []);
+  
+  return (
+    <div>
+      <h1 className="timer text-light">
+        {hours}:{minutes}
+      </h1>
+      <p className="text-light">{day}</p>
+    </div>
+  );
 };
 export { CurrentTime };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,15 @@
+import { CoverImage, CurrentTime } from "../components";
+
 const Homepage = () => {
-  return <>Homepage</>;
+  return (
+    <div>
+      <CoverImage />
+      <div className="main-container br-m">
+        <section className="onboarding-container">
+          <CurrentTime />
+        </section>
+      </div>
+    </div>
+  );
 };
 export { Homepage };
