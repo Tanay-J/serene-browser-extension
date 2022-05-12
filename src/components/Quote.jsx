866440dc188ce6ef37158198cd516c6526b2a8c0
Tanay <tanay.joshi9@gmail.com>
2022-05-12 22:16:33 +0530

    feat, style: new components Quote and Weather

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+import { getRandomQuote } from "../utils/api-requests";
+
 const Quote = () => {
-  return <>Quote</>;
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    getRandomQuote(setQuote);
+    const timer = setInterval(() => getRandomQuote(setQuote), 1800000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="quote-container text-center text-light">
+      <small>{quote.text}</small> -{" "}
+      <small>
+        <em>{quote.author}</em>
+      </small>
+    </div>
+  );
 };
 export { Quote };
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+import { getWeatherUpdate } from "../utils/api-requests";
+
 const Weather = () => {
-  return <>Weather</>;
+  const [weather, setWeather] = useState({
+    city: "",
+    temp: "",
+    icon: "",
+    desc: "",
+  });
+
+  useEffect(() => {
+    getWeatherUpdate(setWeather);
+    const timer = setInterval(() => getWeatherUpdate(setWeather), 3600000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="weather-container">
+      <img className="weather-icon" src={weather.icon} />
+      <div>
+        <h2 className="weather-text text-light">{weather.temp}</h2>
+        <small className="weather-text text-light">{weather.city}</small>
+        <p></p>
+        <small className="weather-text text-light">{weather.desc}</small>
+      </div>
+    </div>
+  );
 };
 export { Weather };
