b9c9b6fa86b1f4dd4b6c69b13818c09f94c7f56b
Tanay <tanay.joshi9@gmail.com>
2022-05-12 14:40:12 +0530

    feat: api services for weather update and random quote

--- src/utils/api-requests.js
+++ src/utils/api-requests.js
@@ -18,3 +18,52 @@ export const getCoverImage = async (setImageData) => {
     console.log(error);
   }
 };
+
+export const getWeatherUpdate = (setWeather) => {
+  const success = async (pos) => {
+    try {
+      let coordinates = {
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+      };
+      let apiURL = `https://fcc-weather-api.glitch.me/api/current?lat=${coordinates.latitude}&lon=${coordinates.longitude}`;
+      let { data } = await axios.get(apiURL);
+      setWeather({
+        city: data.name,
+        temp: data.main.temp + "°C",
+        icon: data.weather[0].icon,
+        desc: data.weather[0].description,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const error = async (err) => {
+    try {
+      let coordinates = { latitude: "28.6139", longitude: "77.2090" };
+      let apiURL = `https://fcc-weather-api.glitch.me/api/current?lat=${coordinates.latitude}&lon=${coordinates.longitude}`;
+      let { data } = await axios.get(apiURL);
+
+      setWeather({
+        city: data.name,
+        temp: data.main.temp + "°C",
+        icon: data.weather[0].icon,
+        desc: data.weather[0].description,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  navigator.geolocation.getCurrentPosition(success, error);
+};
+
+export const getRandomQuote = async (setQuote) => {
+  try {
+    const { data } = await axios.get("https://api.quotable.io/random");
+    setQuote({ text: data.content, author: data.author });
+  } catch (error) {
+    console.log(error);
+  }
+};
